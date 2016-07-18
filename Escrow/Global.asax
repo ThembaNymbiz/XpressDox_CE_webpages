2fd1ec423bbff72a9f40939f6ae648e9b7f1c40e
Themba-Nzimande <themba.nzimande6@gmail.com>
2016-07-18 10:27:34 +0200

    Add project files.

---
+++ Escrow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Escrow.Global" Language="C#" %>
---
+++ Escrow/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Escrow.Startup))]
+namespace Escrow
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
